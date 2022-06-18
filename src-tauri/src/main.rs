cb0cb8b3c6b4e67c63bd41bd1be9657429313ff6
afa <rmingwang@gmail.com>
2022-06-18 20:41:51 +0800

    try 50 ms

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -17,38 +17,76 @@ fn tray_icon() ->Icon {
   Icon::Raw(include_bytes!("../icons/icon.png").to_vec())
 }
 
+#[cfg(target_os = "macos")]
 fn tray_icon_loading() ->Vec<Icon> {
   let mut icon_vec: Vec<Icon> = Vec::new();
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_0.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_1.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_2.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_3.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_4.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_5.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_6.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_7.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_8.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_9.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_10.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_11.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_12.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_13.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_14.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_15.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_16.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_17.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_18.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_19.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_20.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_21.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_22.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_23.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_24.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_25.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_26.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_27.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_28.png").to_vec()));
-  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/loading_29.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_0.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_1.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_2.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_3.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_4.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_5.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_6.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_7.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_8.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_9.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_10.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_11.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_12.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_13.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_14.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_15.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_16.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_17.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_18.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_19.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_20.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_21.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_22.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_23.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_24.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_25.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_26.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_27.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_28.png").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/macos/loading_29.png").to_vec()));
+  icon_vec
+}
+
+
+#[cfg(target_os = "windows")]
+pub fn tray_icon_loading() ->Vec<Icon> {
+  let mut icon_vec: Vec<Icon> = Vec::new();
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_0.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_1.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_2.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_3.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_4.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_5.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_6.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_7.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_8.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_9.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_10.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_11.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_12.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_13.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_14.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_15.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_16.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_17.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_18.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_19.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_20.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_21.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_22.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_23.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_24.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_25.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_26.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_27.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_28.ico").to_vec()));
+  icon_vec.push(Icon::Raw(include_bytes!("../assets/images/windows/loading_29.ico").to_vec()));
   icon_vec
 }
 
@@ -79,7 +117,7 @@ fn tray_event(app: &AppHandle, event: SystemTrayEvent) {
 #[tauri::command]
 async fn set_tray_icon(handle: AppHandle) -> Result<(), String> {
   // loop interval ms
-  let ms = 300;
+  let ms = 50;
   let mut intv = tokio::time::interval(tokio::time::Duration::from_millis(ms));
   let icon_vec = tray_icon_loading();
   tokio::spawn(async move {
